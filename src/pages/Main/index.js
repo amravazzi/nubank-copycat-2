9f16ecfc8c9dd2b049fdefd283e7127431a023ad
Andre Ravazzi <amravazzi@gmail.com>
2021-10-17 01:20:09 -0300

    feat: add main card and fix scroll

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {View, StyleSheet} from 'react-native';
 import type {Node} from 'react';
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 import {Container} from './styles';
+import {
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 function Main(): Node {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 7.654,00</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$20,00 recebida de Fulano hoje às 20h.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
